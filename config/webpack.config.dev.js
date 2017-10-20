b8b4554b6166f377a991af86506ca7c0b18930a7
Mona Rajhans <monarajhans@Monas-MacBook-Pro.local>
2017-10-20 14:56:29 -0700

    Adding sass loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -156,7 +156,7 @@ module.exports = {
           {
             test: /\.scss$/,
             include: paths.appSrc,
-            loaders: ["style-loader", "css-loader"]
+            loaders: ["style-loader", "css-loader", "sass-loader"]
           },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
